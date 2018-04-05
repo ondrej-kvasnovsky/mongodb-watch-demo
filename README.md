b0260e14927f1f87820e7d5111c8ed22a230c2fc
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2018-04-05 16:40:30 -0700

    reafactoring

---
+++ README.md
@@ -0,0 +1,32 @@
+# MongoDB Watch (Chance Stream) Demo
+
+Connects to MongoDB collection and creates [change stream](https://docs.mongodb.com/manual/changeStreams/). 
+Is able to resume using last change stream ID object.  
+
+## Requirements
+
+* nodejs
+* npm
+* mongodb 3.6 and higher
+
+## MongoDB ReplicaSet
+
+MongoDB needs to be started in replica set. 
+Use `mongo-replicaset/setup.sh` to create mongodb using replica set.
+
+After you ran `sh setup.sh`, you need to run: 
+```
+rs.initiate({_id:"rs0", members: [{_id:0, host:"127.0.0.1:27017", priority:100}, {_id:1, host:"127.0.0.1:27018", priority:50}, {_id:2, host:"127.0.0.1:27019", arbiterOnly:true}]})
+```
+
+Connect to database and create `test` database and `items` collection in it.
+
+## How to run the demo
+
+Start the app.js using npm script. 
+Then insert some items into `test.items` collection.
+
+```
+$ npm install
+$ npm start
+```
--- watch-service.js
+++ watch-service.js
@@ -1,41 +1,46 @@
 const fs = require('fs')
 
-const MongoClient = require('mongodb').MongoClient;
-const BSON = require('bson');
-const url = 'mongodb://localhost:27017,127.0.0.1:27018/?replicaSet=rs0';
-const dbName = 'test';
+const MongoClient = require('mongodb').MongoClient
+const BSON = require('bson')
+const url = 'mongodb://localhost:27017,127.0.0.1:27018/?replicaSet=rs0'
+const dbName = 'test'
+const lastIdFileName = 'restore_object.bson'
 
 module.exports = class {
   async start() {
     const client = await MongoClient.connect(url)
-    console.log("Connected!");
+    console.log('Connected!')
     try {
       const db = await client.db(dbName)
       const collection = await db.collection('items')
 
-      // const items = await collection.find().toArray()
-      // console.log(items)
-      console.log(Object.getOwnPropertyNames(BSON.prototype))
-      const recoverId = BSON.prototype.deserialize(fs.readFileSync('lastid.bin'))
-      const changeStream = await collection.watch(
-        [],
-        {
-          resumeAfter: recoverId
-        }
-      )
+      const recoverId = this.getRecoveryObject()
+      const pipeline = [] // filter: insert & update
+      const changeStream = await collection.watch(pipeline, {
+        resumeAfter: recoverId
+      })
       let hasNext = await changeStream.hasNext()
+      //
       while (hasNext) {
-        const next = await changeStream.next()
-        let lastId = BSON.prototype.serialize(next._id);
-        fs.writeFileSync('lastid.bin', lastId)
-        console.log(lastId)
-        console.log(next.fullDocument)
+        const message = await changeStream.next()
+        const lastId = BSON.prototype.serialize(message._id)
+        fs.writeFileSync(lastIdFileName, lastId)
+        console.log(message.fullDocument)
         hasNext = await changeStream.hasNext()
       }
       console.log('Closing connection...')
-      await changeStream.close();
+      await changeStream.close()
     } catch (e) {
       console.log(e)
     }
+  }
+
+  getRecoveryObject() {
+    let recoverId = null
+    if (fs.existsSync(lastIdFileName)) {
+      const lastIdFile = fs.readFileSync(lastIdFileName)
+      recoverId = BSON.prototype.deserialize(lastIdFile)
+    }
+    return recoverId
   }
 }
