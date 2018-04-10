dfd7986c31e079f9faf0cb36be4be61a4bf98db9
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2018-04-10 09:23:06 -0700

    added buffer

--- watch-service.js
+++ watch-service.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
+const Rx = require('rxjs/Rx')
 
 const MongoClient = require('mongodb').MongoClient
 const BSON = require('bson')
-const url = 'mongodb://localhost:27017,127.0.0.1:27018/?replicaSet=rs0'
+// const url = 'mongodb://localhost:27017,127.0.0.1:27018/?replicaSet=rs0'
+const url = 'mongodb://10.99.129.132:27017/?replicaSet=rs0'
 const dbName = 'test'
 const lastIdFileName = 'restore_object.bson'
 
@@ -14,33 +16,87 @@ module.exports = class {
       const db = await client.db(dbName)
       const collection = await db.collection('items')
 
-      const recoverId = this.getRecoveryObject()
-      const pipeline = [
-        {
-          $match: {
-            $or: [{ operationType: 'insert' }, { operationType: 'delete' }, { operationType: 'update' }]
-          }
-        }
-      ]
-      const options = {
-        resumeAfter: recoverId
+      this.readExisting(collection)
+      this.startChangeStream(collection)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async readExisting(collection) {
+    // const lastItem = await collection
+    //   .find()
+    //   .sort({ $natural: 1 })
+    //   .limit(1)
+    //   .next()
+    // console.log('last item')
+    // console.log(lastItem)
+    // {_id: {$gt: lastItem._id}}
+    const cursor = await collection.find().batchSize(10)
+    console.log('Reading existing items...')
+    const observable = Rx.Observable.create(async observer => {
+      while (await cursor.hasNext()) {
+        observer.next(await cursor.next())
       }
-      const changeStream = await collection.watch(pipeline, options)
+      cursor.close()
+      observer.complete()
+    }).bufferCount(10)
+    const subscription = observable.subscribe(
+      value => {
+        console.log('Push existing items to Firehose', value)
+      },
+      err => {
+        console.log(err)
+      },
+      async () => {
+        console.log('this is the end')
+        subscription.unsubscribe()
+      }
+    )
+    setTimeout(() => {
+      console.log('Timed out, unsubscribe!')
+      subscription.unsubscribe()
+    }, 24 * 60 * 60 * 1000)
+  }
+
+  async startChangeStream(collection) {
+    const recoverId = this.getRecoveryObject()
+    const pipeline = [
+      {
+        $match: {
+          $or: [{ operationType: 'insert' }, { operationType: 'delete' }, { operationType: 'update' }]
+        }
+      }
+    ]
+    const options = {
+      resumeAfter: recoverId
+    }
+    const changeStream = await collection.watch(pipeline, options)
+
+    const observable = Rx.Observable.create(async observer => {
       let hasNext = await changeStream.hasNext()
       while (hasNext) {
         const event = await changeStream.next()
-        const lastId = BSON.prototype.serialize(event._id)
-        fs.writeFileSync(lastIdFileName, lastId)
-        console.log(event.ns)
-        console.log(event.operationType)
-        console.log(event.fullDocument)
+        observer.next(event)
         hasNext = await changeStream.hasNext()
       }
       console.log('Closing connection...')
       await changeStream.close()
-    } catch (e) {
-      console.log(e)
-    }
+      observer.complete()
+    }).bufferCount(10)
+    const subscription = observable.subscribe(
+      value => {
+        const lastId = BSON.prototype.serialize(value[value.length - 1]._id)
+        fs.writeFileSync(lastIdFileName, lastId)
+        console.log('Stream to Firehose', value.length)
+      },
+      err => {
+        console.log(err)
+      },
+      async () => {
+        console.log('End?')
+      }
+    )
   }
 
   getRecoveryObject() {
